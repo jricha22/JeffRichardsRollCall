667165532f993a7d119e5a9f85761359bac94da3
Jeff Richards <jeff.richards@earthlink.net>
2016-04-24 14:16:04 -0400

    Implement statistics calculations and display

--- app/src/main/java/edu/westga/jeffrichardsrollcall/view/ManageActivity.java
+++ app/src/main/java/edu/westga/jeffrichardsrollcall/view/ManageActivity.java
@@ -2,25 +2,22 @@ package edu.westga.jeffrichardsrollcall.view;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.TextWatcher;
 import android.text.Editable;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.HashMap;
 
 import edu.westga.jeffrichardsrollcall.R;
+import edu.westga.jeffrichardsrollcall.model.Attendance;
 import edu.westga.jeffrichardsrollcall.model.DatabaseHandler;
 
 public class ManageActivity extends AppCompatActivity {
@@ -31,7 +28,7 @@ public class ManageActivity extends AppCompatActivity {
     private Button manageStudents;
     private Button addClass;
     private EditText newClassName;
-    private ArrayAdapter<String> classAdapter;
+    private ClassListViewAdapter classAdapter;
     private DatabaseHandler myDbHandler;
 
     @Override
@@ -44,9 +41,8 @@ public class ManageActivity extends AppCompatActivity {
         this.addClass = (Button) findViewById(R.id.btnAddClass);
         this.newClassName = (EditText) findViewById(R.id.txtClassName);
         this.myDbHandler = new DatabaseHandler(this.getApplicationContext());
-        ArrayList<String> classes = new ArrayList<>();
-        this.classAdapter = new ArrayAdapter<>(this, R.layout.class_listview, classes);
-        this.classAdapter.setNotifyOnChange(true);
+        ArrayList<HashMap<String, String>> classes = new ArrayList<>();
+        this.classAdapter = new ClassListViewAdapter(this, classes);
         this.classList.setAdapter(this.classAdapter);
 
         this.populateClasses();
@@ -86,14 +82,21 @@ public class ManageActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onResume(){
+        super.onResume();
+        this.populateClasses();
+    }
+
     private void populateClasses() {
-        ArrayList<String> newClasses = this.myDbHandler.getAllClasses();
+        ArrayList<Attendance> newClasses = this.myDbHandler.getAllClasses();
         this.classAdapter.clear();
-        for (String aClass: newClasses) {
-            this.classAdapter.add(aClass);
+        for (Attendance aClass: newClasses) {
+            this.classAdapter.add(aClass.getName(), Integer.toString(aClass.getNumClasses()), Integer.toString(aClass.getAttendencePercent()) + "%");
         }
         this.deleteClass.setEnabled(false);
         this.manageStudents.setEnabled(false);
+        this.classList.setAdapter(this.classAdapter);
     }
 
     public void onAddClass(View view) {
@@ -107,14 +110,18 @@ public class ManageActivity extends AppCompatActivity {
         this.newClassName.getText().clear();
     }
 
+    @SuppressWarnings("unchecked")
     public void onDeleteClass(View view) {
-        String itemToDelete = this.classList.getItemAtPosition(this.classList.getCheckedItemPosition()).toString();
+        HashMap<String, String> item = (HashMap<String, String>)this.classList.getItemAtPosition(this.classList.getCheckedItemPosition());
+        String itemToDelete = item.get("first");
         this.myDbHandler.deleteClass(itemToDelete);
         this.populateClasses();
     }
 
+    @SuppressWarnings("unchecked")
     public void onManageStudents(View view) {
-        String itemToManage = this.classList.getItemAtPosition(this.classList.getCheckedItemPosition()).toString();
+        HashMap<String, String> item = (HashMap<String, String>)this.classList.getItemAtPosition(this.classList.getCheckedItemPosition());
+        String itemToManage = item.get("first");
         Intent intent = new Intent(this, ManageStudentsActivity.class);
         intent.putExtra(CLASS_SELECTION, itemToManage);
         startActivity(intent);
